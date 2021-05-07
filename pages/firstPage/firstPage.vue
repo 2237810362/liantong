4a91b92345aabf7936dc88535d22a9937ae58c79
wangyuhua <2237810362@qq.com>
2021-05-07 21:29:44 +0800

    4

--- pages/firstPage/firstPage.vue
+++ pages/firstPage/firstPage.vue
@@ -1,6 +1,25 @@
 <template>
-	<view class="">
-		优选首页
+	<view class="container">
+		<view class="top_line">
+			<view class="card">
+				<view class="t">
+					<view class="l">
+						<text>附近商户：</text>
+						<text>可可超市</text>
+						<u-icon name="arrow-right" size="40rpx" color="#909399"></u-icon>
+					</view>
+					<view class="r">
+						<image src="../../static/imgs/change.png" mode=""></image>
+						<text>切换</text>
+					</view>
+				</view>
+				<view class="b">
+					<text>天府大道北段1700号附134环…</text>
+					<text>|</text>
+					<text>距您809米</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,4 +27,17 @@
 </script>
 
 <style>
+	.top_line{
+		padding: 0 24rpx;
+		padding-top: 24rpx;
+		height: 96rpx;
+		background-color: #F67220;
+	}
+	.top_line .card{
+		height: 140rpx;
+		background: #FFFFFF;
+		box-shadow: 0rpx 4rpx 12rpx 0rpx rgba(0,0,0,0.06);
+		border-radius: 16rpx;
+		padding: 24rpx;
+	}
 </style>
